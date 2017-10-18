58c17e070891e02fa1ad567578a81e5d4dbe1566
Sudhansu nayak <sekharinweb@gmail.com>
2017-10-18 13:27:13 +0530

    useless entity added.

---
+++ src/web.api.entities/UseLess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web.api.entities
+{
+    public class UseLess
+    {
+        public int UseLessId { get; set; }
+        public string UseLessName { get; set; }
+        public string UseLessDescription { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string UselessHref { get; set; }
+    }
+}
