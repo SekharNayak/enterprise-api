56f7d4c78de6ee4bc82e0affc713ac34011209ef
Sudhansu nayak <sekharinweb@gmail.com>
2017-10-21 17:55:22 +0530

    filters added

---
+++ src/web.api.formatters/JsonpFormatter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web.api.formatters
+{
+    public class JsonpFormatter : MediaTypeFormatter
+    {
+        const string Jsonp_Media_Type = "Application/JSONP";
+        public JsonpFormatter()
+        {
+            this.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue(Jsonp_Media_Type));
+        }
+        public override bool CanReadType(Type type)
+        {
+            return false;
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+
+            return true;
+        }
+
+        public override Task WriteToStreamAsync(Type type, object value, 
+            Stream writeStream, HttpContent content, TransportContext transportContext)
+        {
+
+            return base.WriteToStreamAsync(type, value, writeStream, content, transportContext);
+        }
+    }
+}
